871e56acd0ff9d5aabae5befd4e67b0a0756a2c1
Ari Bambang K <aribk210@gmail.com>
2018-11-07 01:04:26 +0700

    adding validation user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,16 +5,31 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const Joi = require('joi');
+
 module.exports = {
   
-
+  
   /**
    * `UserController.signup()`
    */
   signup: async function (req, res) {
-    return res.json({
-      todo: 'signup() is not implemented yet!'
-    });
+    try {
+     
+      const schema = Joi.object().keys({
+        email: Joi.string().required().email(),
+        password: Joi.string().required()
+      });
+
+      const params = await Joi.validate(req.allParams(), schema);
+
+      return res.ok(params);
+    } catch (error) {
+      if(error.name === 'ValidationError'){
+        return res.badRequest({error});
+      }
+      return res.serverError(error);
+    }
   },
 
   /**
